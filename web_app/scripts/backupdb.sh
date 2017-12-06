7f4b011e19e58436845ec3fabcbdbda3bcb3ea6b
Eko Wibowo <eko@monetizemore.com>
2017-12-07 05:37:04 +0700

    DONE: db backup
    DEPLOYMENT ISSU:
    - Url for page is not getting updated to safe url
    - Allow the use of external image as feature image
    
    BACKLOG:
    - Indonesia locale for humanize
    - keyword field for SEO purposes
    - soccer API! News!
    - fix SERVER_NAME in localhost
    - tambahkan back to top icon in footer
    - bootstrap init for psql
    - Need better prod/dev setup
    - spice Up NM brand
    - / for with Home Icon Menu
    - Prepare until ready for NM
    - CAN DB AUTO GENERATED BY resetmigration.sh script?
    - Need to remove that web_app reference in import. Not pretty
    - Make the uploader be ajax friendly from ckeditor
    - Excerpt for post to be shown in thumbnail (manual field?)
    - show profile menu for authenticated user. For now, just hide it
    - , view count

--- web_app/scripts/backupdb.sh
+++ web_app/scripts/backupdb.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
 pg_dump --dbname=postgresql://devuser:devpassword@localhost:5434/web_app > ~/db.sql
+
+# copy from DO to local
+# scp root@nm:~/db.sql .
+
+# to restore db
+
+# stop web_app and exit from any app using it (such as pgadmin3)
+# sudo docker-compose stop web_app
+
+# drop db
+# dropdb -h localhost -p 5434 -U devuser web_app
+
+# load db
+# psql -h localhost -p 5434 -U devuser web_app < db.sql
