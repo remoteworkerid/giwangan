1a70ca5bdbf3689e57b0120e8ffb4fd89d3beda0
Eko Wibowo <eko@monetizemore.com>
2017-12-09 12:11:48 +0700

    Fix static cache
    BACKLOG:
    - Probably need to fix the vertical alignment of brand logo when in mobile
    - Custom Home Admin and POC for create article based on keyword
    - Url for page is not getting updated to safe url (?)
    - Allow the use of external image as feature image
    - Nawarin berita politik,"Anies/sandi atau Ahok/Djarot? Islam moderat/liberal?" bwahaha
    - Twitter/Facebook/LinkedIn bot
    - Indonesia locale for humanize
    - keyword field for SEO purposes
    - soccer API! News!
    - fix SERVER_NAME in localhost
    - tambahkan back to top icon in footer
    - bootstrap init for psql
    - Need better prod/dev setup
    - spice Up NM brand
    - / for with Home Icon Menu
    - Prepare until ready for NM
    - CAN DB AUTO GENERATED BY resetmigration.sh script?
    - Need to remove that web_app reference in import. Not pretty
    - Make the uploader be ajax friendly from ckeditor
    - Excerpt for post to be shown in thumbnail (manual field?)
    - show profile menu for authenticated user. For now, just hide it
    - , view count

--- web_app/settings.py
+++ web_app/settings.py
@@ -5,9 +5,9 @@ GREETING = 'Halo Dunia. Apa kabar?'
 
 SQLALCHEMY_DATABASE_URI = 'postgresql://devuser:devpassword@postgres:5432/web_app'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-
 SECRET_KEY = 'marvelisgreaterthandc'
 
+#Flask-security
 SECURITY_REGISTERABLE = True
 SECURITY_PASSWORD_SALT = 'https://stackoverflow.com/questions/25942092/unique-salt-per-user-using-flask-security'
 
@@ -18,6 +18,9 @@ SECURITY_SEND_REGISTER_EMAIL = False
 
 #once register, set this as admin
 ADMIN_ACCOUNT = 'swdev.bali@gmail.com'
+
+#development, set 0 to refresh static cache
+SEND_FILE_MAX_AGE_DEFAULT = 0
 
 import os
 import os.path as op
