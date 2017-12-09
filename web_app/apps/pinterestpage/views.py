dc8d0753c6c94b3ce6f9c2a9d41b74b2384b1cff
Eko Wibowo <eko@monetizemore.com>
2017-12-09 09:26:33 +0700

    DONE: Show / hide correct brand logo when in mobile mode. Probably need to fix the vertical alignment
    BACKLOG:
    - Probably need to fix the vertical alignment of brand logo when in mobile
    - Custom Home Admin and POC for create article based on keyword
    - Url for page is not getting updated to safe url (?)
    - Allow the use of external image as feature image
    - Nawarin berita politik,"Anies/sandi atau Ahok/Djarot? Islam moderat/liberal?" bwahaha
    - Twitter/Facebook/LinkedIn bot
    - Indonesia locale for humanize
    - keyword field for SEO purposes
    - soccer API! News!
    - fix SERVER_NAME in localhost
    - tambahkan back to top icon in footer
    - bootstrap init for psql
    - Need better prod/dev setup
    - spice Up NM brand
    - / for with Home Icon Menu
    - Prepare until ready for NM
    - CAN DB AUTO GENERATED BY resetmigration.sh script?
    - Need to remove that web_app reference in import. Not pretty
    - Make the uploader be ajax friendly from ckeditor
    - Excerpt for post to be shown in thumbnail (manual field?)
    - show profile menu for authenticated user. For now, just hide it
    - , view count

--- web_app/apps/pinterestpage/views.py
+++ web_app/apps/pinterestpage/views.py
@@ -11,11 +11,18 @@ def process(page):
     if data['show_post']:
         global_vars.ACTIVE_MENU = page.title
         if 'tag' in data and data['tag']: # tag with feature, will be in homepage
-            posts = db.session.query(Page).join(PageState).filter(PageState.title=='Published', Page.tag.contains(data['tag']))\
+            posts = db.session.query(Page).join(PageState).\
+                filter(PageState.title == 'Published',
+                       Page.tag.contains(data['tag']),
+                       Page.subtype == 'page')\
                 .order_by(Page.stamp.desc()).all()
 
         elif 'category' in data and data['category']: # while 'category' will contains post for that category
-            posts = db.session.query(Page).join(PageState).filter(PageState.title=='Published', Page.category == data['category']).order_by(Page.stamp.desc()).all()
+            posts = db.session.query(Page).join(PageState).\
+                filter(PageState.title=='Published',
+                       Page.category == data['category'],
+                       Page.subtype == 'page').\
+                order_by(Page.stamp.desc()).all()
     return render_template('pinterestpage/content.html', posts=posts, global_vars=global_vars)
 
 def get_og(page):
