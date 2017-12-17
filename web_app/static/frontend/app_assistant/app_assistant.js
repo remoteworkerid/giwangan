1250f4a78ee8448fa70b57dacd93106cd377e51e
Eko Wibowo <eko@monetizemore.com>
2017-12-17 07:39:13 +0700

    WIP: React element JS, no JSX
    BACKLOG:
    - csrf need to be activated
    - Need to add member who view certain page
    - Probably need to fix the vertical alignment of brand logo when in mobile
    - Custom Home Admin and POC for create article based on keyword
    - Url for page is not getting updated to safe url (?)
    - Allow the use of external image as feature image
    - Nawarin berita politik,"Anies/sandi atau Ahok/Djarot? Islam moderat/liberal?" bwahaha
    - Twitter/Facebook/LinkedIn bot
    - Indonesia locale for humanize
    - keyword field for SEO purposes
    - soccer API! News!
    - fix SERVER_NAME in localhost
    - tambahkan back to top icon in footer
    - bootstrap init for psql
    - Need better prod/dev setup
    - spice Up NM brand
    - / for with Home Icon Menu
    - Prepare until ready for NM
    - CAN DB AUTO GENERATED BY resetmigration.sh script?
    - Need to remove that web_app reference in import. Not pretty
    - Make the uploader be ajax friendly from ckeditor
    - Excerpt for post to be shown in thumbnail (manual field?)
    - show profile menu for authenticated user. For now, just hide it
    - , view count

---
+++ web_app/static/frontend/app_assistant/app_assistant.js
@@ -0,0 +1,12 @@
+const square = React.createElement('div', {
+  style: {
+    width: '100px',
+    height: '100px',
+    backgroundColor: 'red',
+    margin: '10px',
+  }
+})
+ReactDOM.render(
+  square,
+  document.getElementById('app_assistant_root')
+)
