26487c60bb3c6d61edf391f82d8bebdcd08e1a90
Eko Wibowo <eko@monetizemore.com>
2017-12-17 23:00:20 +0700

    WIP: Babel done it, ReactDOM.render okay, component okay, form also okay NEXT: create each comp for assistant gui
    BACKLOG:
    - csrf need to be activated
    - Need to add member who view certain page
    - Probably need to fix the vertical alignment of brand logo when in mobile
    - Custom Home Admin and POC for create article based on keyword
    - Url for page is not getting updated to safe url (?)
    - Allow the use of external image as feature image
    - Nawarin berita politik,"Anies/sandi atau Ahok/Djarot? Islam moderat/liberal?" bwahaha
    - Twitter/Facebook/LinkedIn bot
    - Indonesia locale for humanize
    - keyword field for SEO purposes
    - soccer API! News!
    - fix SERVER_NAME in localhost
    - tambahkan back to top icon in footer
    - bootstrap init for psql
    - Need better prod/dev setup
    - spice Up NM brand
    - / for with Home Icon Menu
    - Prepare until ready for NM
    - CAN DB AUTO GENERATED BY resetmigration.sh script?
    - Need to remove that web_app reference in import. Not pretty
    - Make the uploader be ajax friendly from ckeditor
    - Excerpt for post to be shown in thumbnail (manual field?)
    - show profile menu for authenticated user. For now, just hide it
    - , view count

--- web_app/static/frontend/app_assistant/app_assistant.js
+++ web_app/static/frontend/app_assistant/app_assistant.js
@@ -1,26 +1,80 @@
-const ce = React.createElement;
+var ContentAssistant = React.createClass({
 
+  getInitialState: function() {
+    return { url: '' };
+  },
 
-const square = ce('div', {
-    style: {
-        width: '100px',
-        height: '100px',
-        backgroundColor: 'green',
-        margin: '10px'
+  handleChange(event){
+    this.setState({url: event.target.value})
+  },
+
+  render: function(){
+    return (
+      <div className="input-group">
+        <input placeholder="URL" id="txt_url" className="form-control"
+        value={this.state.url} onChange={this.handleChange.bind(this)}/>
+        <span className="input-group-btn">
+          <button className="btn btn-default value-control" id="btn_fetch" onClick={this.onClick}>
+          <span className="glyphicon glyphicon-download"></span>
+          </button>
+        </span>
+      </div>
+    );
+  },
+
+  onClick: function(){
+    console.log('onClick ' + this.state.url);
+    $.ajax({
+      type: "GET",
+      url: "/api/assistant",
+      data: {'url': this.state.url},
+      dataType: "json",
+      success: function (data, status, jqXHR) {
+        data = JSON.parse(data);
+        console.log(data);
+        // TODO create component for each title/text/images for this Assistant
+        // BUT Focus on M2 first in the morning yea
+        // $('#result').append(data.result.title);
+        // text = ' \
+        //         <div class="input-group"> \
+        //         <textarea id="txt_result" class="form-control">' + data.result.text + '</textarea> \
+        //     </div>';
+        // $('#result').append(text);
+
+      },
+
+      error: function (jqXHR, status) {
+        console.log(jqXHR);
+
       }
+    });
+
+  },
 });
 
-const container = ce('div', {
-    style: {
-        border: '5px solid green'
-        }
-    },
-    'Rects',
-    square,
-    square
-);
-
 ReactDOM.render(
-  container,
+  <ContentAssistant/>,
   document.getElementById('app_assistant_root')
 );
+
+/*
+ var RandomMessage = React.createClass({
+ getInitialState: function() {
+ return { message: 'Hello, Universe' };
+ },
+ onClick: function() {
+ var messages = ['Hello, World', 'Hello, Planet', 'Hello, Universe'];
+ var randomMessage = messages[Math.floor((Math.random() * 3))];
+
+ this.setState({ message: randomMessage });
+ },
+ render: function() {
+ return (
+ <div>
+ <MessageView message={ this.state.message }/>
+ <p><input type="button" onClick={ this.onClick } value="Change Message"/></p>
+ </div>
+ );
+ }
+ });
+ */
